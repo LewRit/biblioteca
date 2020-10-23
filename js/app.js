a95da699b1743091293e0787ff9f4a44641c6a52
LewRit <lewrit16@gmail.com>
2020-10-22 21:30:51 -0400

    added form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-$().ready(()=>{
-    console.log('app.js is running')
-})
+const stringifyFormData=fd=>{
+    const data={};
+    for (let field of fd.keys()){
+        data[field]=fd.get(field);
+    }
+    return JSON.stringify(data,null,2)
+}
+const handleSubmit=e=>{
+    e.preventDefault();
+    const data=new FormData(e.target)
+    const stringified=stringifyFormData(data);
+    console.log(stringified)
+}
+const form =document.getElementById('login')
+form.addEventListener('submit', handleSubmit)
